6d917d97fb11bc43062faeb7b5f293d69463ca51
Chau <hoaichau20@hotmail.com>
2026-02-23 10:23:00 +0100

    first save

---
+++ Back/serveur.js
@@ -0,0 +1,25 @@
+import "dotenv/config";
+import express from "express";
+import cors from "cors";
+import { neon } from "@neondatabase/serverless";
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+const PORT = process.env.PORT || 4242;
+
+app.get("/themes", async (req, res) => {
+  try {
+    const sql = neon(`${process.env.DATABASE_URL}`);
+    const response = await sql`SELECT * FROM themes`;
+    console.log(response);
+    res.json(response);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "erreur du serveur" });
+  }
+});
+
+app.listen(PORT, () => {
+  console.log(`Listening to http://localhost:${PORT}`);
+});
