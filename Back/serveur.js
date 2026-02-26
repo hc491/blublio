8ae70c45207b53d332db072d097f20d93bd71166
Chau <hoaichau20@hotmail.com>
2026-02-26 14:34:23 +0100

    themes+map

--- Back/serveur.js
+++ Back/serveur.js
@@ -14,6 +14,7 @@ app.get("/themes", async (req, res) => {
     const response = await sql`
     SELECT *
     FROM themes
+    ORDER BY name ASC
     `;
     console.log(response);
     res.json(response);
@@ -31,7 +32,7 @@ app.get("/items/:libelle", async (req, res) => {
     const response = await sql`
     SELECT *
     FROM items
-    WHERE libelle ILIKE ${'%'+libelle+'%'}`;//insensible à la casse
+    WHERE libelle ILIKE ${`%${libelle}%`}`;//insensible à la casse
     console.log(response);
     res.json(response);
   } catch (error) {
@@ -48,6 +49,23 @@ app.get("/themes/:id/items", async (req, res) => {
     SELECT *
     FROM items
     WHERE themes_id = ${id}
+    ORDER BY libelle ASC
+    `;
+    console.log(response);
+    res.json(response);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "erreur du serveur" });
+  }
+});
+
+app.post("/themes", async (req, res) => {
+  try {
+    const {name} = req.body;
+    const sql = neon(`${process.env.DATABASE_URL}`);
+    const response = await sql`
+    INSERT INTO themes (name)
+    VALUES (${name})
     `;
     console.log(response);
     res.json(response);
